e81cee79e814de60bb6bdd25b2944f9faa826cd2
nbaptist16 <nbaptist16@punahou.edu>
2015-01-11 19:50:49 -1000

    RUN
    Since it didn’t seem like anyone minded that it wasn’t a real-time
    game, as I had originally planned, I decided to leave it as it is
    (instead of risking messing it up)
    Oh, and I fixed the bugs :D

--- RUN.ino
+++ RUN.ino
@@ -1,5 +1,11 @@
 /*
   RUN.pde
+  
+  
+   DIRECTIONS TO PLAY:
+       - Use the control pad to switch directions
+           
+  
  
  Example file using the The Meggy Jr Simplified Library (MJSL)
   from the Meggy Jr RGB library for Arduino
@@ -43,13 +49,13 @@
 int mazeArray[25][28] =
 {
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,4,4,4,0,0,0},  // maze
-  {0,4,4,4,4,0,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4},  // but as of now it is flipped
+  {0,4,4,4,4,0,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4},  // but it is flipped
   {0,4,0,0,4,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,4,4,0,0,0,0,0,4},  // and rotated 90ª to the right
   {0,4,0,0,4,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,4,4,0,0,0,0,0,4},  // ;-;
   {0,4,0,0,4,0,4,0,0,4,4,4,4,4,4,4,4,4,0,0,4,4,0,1,4,0,0,4},
   {0,4,0,0,4,0,4,0,0,4,0,0,0,0,0,0,0,4,0,0,4,4,0,1,4,0,1,4},
   {0,4,0,0,4,0,4,0,0,4,0,0,0,0,0,0,0,4,0,0,4,4,0,0,4,0,1,4},
-  {0,4,0,0,4,0,4,0,0,4,4,4,4,4,4,0,0,4,0,0,4,4,0,0,4,0,0,4},
+  {0,4,0,0,4,0,4,0,0,4,4,4,4,4,4,0,0,4,0,0,4,4,0,0,4,12,0,4},
   {0,4,0,0,4,0,4,0,0,0,0,0,0,0,4,0,0,4,0,0,4,4,0,0,4,4,4,4},
   {0,4,0,0,4,0,4,0,0,0,0,0,0,0,4,0,0,4,0,0,4,4,0,1,4,0,0,0},
   {0,4,0,1,4,0,4,4,4,4,4,4,0,0,4,0,0,4,1,0,4,4,0,1,4,0,0,0},
@@ -70,7 +76,7 @@ int mazeArray[25][28] =
 
 };
 
-int offsetX = 0;
+int offsetX = 0;  //player's distance from starting point
 int offsetY = 0;
 
 void setup()                    // run once, when the sketch starts
@@ -81,91 +87,145 @@ void setup()                    // run once, when the sketch starts
 
 // Global variables go here...
 
-/*
-Point mazeArray[24row][24column] =
-{
-  {#,#<#...#},
-  {#,#...
-}
-*/
-
-
-
-// below: DrawPx (x,y,screen[x][y]}
-/*
-for (int i = 0; i < 576; i++)
-{
-  if (mazeArray[i].x >= 0 &&
-      mazeArray[i].x <= 7 &&
-      mazeArray[i].y >= 0 &&
-      mazeArray[i].y <= 7)
-  {
-    DrawPx(mazeArray[i].x, mazeArray[i].y, Green);
-  }
-}
-*/
-
-
-
-
 
 
 void loop()                     // run over and over again
 { 
   ClearSlate();
-  for (int i = 0; i < 7; i++)
+  for (int i = 0; i < 7; i++)  // to only display what's within the screen...
   {
     for (int j = 0; j < 7; j++)
     {
-      int x = i + offsetX;
+      int x = i + offsetX;    // the PLAYER'S position in regard to the array
       int y = j + offsetY;
-      /*
-      Serial.print("i is ");
-      Serial.println(i);  //ln = line
-      Serial.print("j is ");
-      Serial.println(j);
-      Serial.println();
-     */
-      DrawPx(i,j, mazeArray[x][y]);
+     
+      DrawPx(i,j, mazeArray[x][y]);  // to display the array
     }
   }
   
   DrawPx(2,2,Yellow);  // draw PLAYER over background
   DisplaySlate();
   
-  CheckButtonsPress();
+  CheckButtonsPress();  // w/ help from Mr. Kiang
+        // for descriptions of what buttons do, see Button_Right
   
   if (Button_Right)
   {
-    offsetX++;
+    if (ReadPx(3,2) == 0)  // if space to the right is "empty"
+    {
+      offsetX++;    // move to the right
+    }
+    else
+    if (ReadPx(3,2) == 1)
+    {
+      Tone_Start(ToneD3, 250);
+    }
+    else
+    if (ReadPx(3,2) == 4)
+    {
+      Tone_Start(ToneD3, 250);
+    }
+    else
+    if (ReadPx (3,2) == 12)
+    {
+      offsetX++;
+      Tone_Start(ToneG4,250);
+      while (MakingSound) {}
+      delay(25);
+      Tone_Start(ToneA4,150);
+      while (MakingSound) {}
+      delay(25);
+      Tone_Start(ToneG4,150);
+      while (MakingSound) {}
+      delay(25);
+      Tone_Start(ToneD5,450);
+      while (MakingSound) {}
+      delay(50);
+      // got above searching for a catchy theme to play on the meggy
+          //http://individual.utoronto.ca/owlman9000/meggyjr/meggyjr_reverse_game.pde
+      
+      for (int x=0; x < 8; x++)
+      {
+        DisplaySlate();
+        for (int i=0; i < 8; i++)
+        {
+          if ((i % 2) == 0)
+          {
+             if ((x % 2) != 0)
+             {
+                DrawPx(i,x,FullOn);
+                DisplaySlate();
+                delay(50); 
+              }
+              else
+                {
+                  DrawPx(i,x,Blue);
+                  DisplaySlate();
+                  delay(50);
+                 }
+                    
+          }
+          else
+          {
+            DrawPx(i,x,Green);
+            DisplaySlate();
+            delay(50);
+          }
+        }
+      }
+     
+    }
   }
   if (Button_Left)
   {
+    if (ReadPx(1,2) == 0)
+    {
     offsetX--;
+    }
+    else
+    if (ReadPx(1,2) == 1)
+    {
+      Tone_Start(ToneD3, 250);
+    }
+    else
+    if (ReadPx(1,2) == 4)
+    {
+      Tone_Start(ToneD3, 250);
+    }
   }
   if (Button_Up)
   {
+    if (ReadPx (2,3) == 0)
+    {
     offsetY++;
+    }
+    else
+    if (ReadPx (2,3) == 1)
+    {
+      Tone_Start(ToneD3, 250);
+    }
+    else
+    if (ReadPx (2,3) == 4)
+    {
+      Tone_Start(ToneD3, 250);
+    }
   }
   if (Button_Down)
   {
+    if (ReadPx (2,1) == 0)
+    {
     offsetY--;
+    }
+    else
+    if (ReadPx (2,1) == 1)
+    {
+      Tone_Start(ToneD3, 250);
+    }
+    else
+    if (ReadPx (2,1) == 4)
+    {
+      Tone_Start(ToneD3, 250);
+    }
   }
-  
-    
-  if (ReadPx (2,2) == Green)  // lose lives
-  {
-    Tone_Start(ToneG3, 250);
-    Tone_Start(ToneFs3, 250);
-  }
-   
-  
-  // if player is on "warp panel" (25,7), warp back to start (2,2); speed++
-  
-  // if player is at mazeArray [x][y] == Green > die (restart)
-    // play tone: d, c#, a#
-    
-    /*above = pseudo code since I'm having touble since technically the player isn't
-    moving and the maze is an array*/
   
 }
